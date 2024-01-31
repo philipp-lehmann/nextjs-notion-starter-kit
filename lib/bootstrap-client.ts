c7a27533198c362030e3aa763434623d48fc4a3e
Philipp Lehmann <philipp@redefine.studio>
2024-01-31 21:15:48 +0100

    Testing update

--- lib/bootstrap-client.ts
+++ lib/bootstrap-client.ts
@@ -1,12 +1,3 @@
 export function bootstrap() {
-  console.log(`
-
-  ___ ___  ___ _  _ 
- | _ \   \| __| \| |
- |   / |) | _|| .` |
- |_|_\___/|_| |_|\_|
-                    
-                                      
-   This site is built using Notion, Next.js, and https://github.com/NotionX/react-notion-x.
-`)
+  console.log(`RDFN - This site is built using Notion, Next.js, and https://github.com/NotionX/react-notion-x.`)
 }
